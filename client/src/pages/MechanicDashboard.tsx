3eb5a3d5a3fbdb427ae3117ac116da070fd4ab46
mxk <marek.kulla89@gmail.com>
2026-01-16 19:00:22 +0100

    Fix status names

--- client/src/pages/MechanicDashboard.tsx
+++ client/src/pages/MechanicDashboard.tsx
@@ -134,20 +134,22 @@ function MechanicDashboard() {
                   </p>
 
                   <div className="gap-2 mt-4 grid sm:grid-cols-2">
-                    {a.status !== "V riešení" && a.status !== "Zrušená" && (
+                    {a.status !== "Začatá" &&
+                      a.status !== "Zrušená" &&
+                      a.status !== "Ukončená" && (
+                        <button
+                          onClick={() =>
+                            updateStatus(a.appointment_id, "Začatá")
+                          }
+                          className="flex-1 px-3 py-2 bg-blue-100 text-blue-600 rounded-lg hover:bg-blue-200 font-medium"
+                        >
+                          Začať opravu
+                        </button>
+                      )}
+                    {a.status !== "Ukončená" && a.status !== "Zrušená" && (
                       <button
                         onClick={() =>
-                          updateStatus(a.appointment_id, "V riešení")
-                        }
-                        className="flex-1 px-3 py-2 bg-blue-100 text-blue-600 rounded-lg hover:bg-blue-200 font-medium"
-                      >
-                        Začať opravu
-                      </button>
-                    )}
-                    {a.status !== "Dokončená" && a.status !== "Zrušená" && (
-                      <button
-                        onClick={() =>
-                          updateStatus(a.appointment_id, "Dokončená")
+                          updateStatus(a.appointment_id, "Ukončená")
                         }
                         className="flex-1 px-3 py-2 bg-green-100 text-green-600 rounded-lg hover:bg-green-200 font-medium"
                       >
